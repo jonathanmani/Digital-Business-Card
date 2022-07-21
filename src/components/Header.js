9ebed383c99c39f006237d42e6c78e8c54506247
Jonathan Mani <jonathanmani@protonmail.com>
2022-07-21 12:57:51 -0400

    added the Header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header(){
+    return(
+        <div>
+            <h1>Jonathan Mani</h1>
+            <h3>Full Stack Developer</h3>
+            <h4><a href='https://jonathanmani.netlify.app/'>Portfolio</a></h4>
+        </div>
+    )
+}
---
+++ src/components/Photo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Photo(){
+    return (
+        <img src={"./src/images/orange3.png"} alt='profile-pics' className='image'/>
+    )
+}
