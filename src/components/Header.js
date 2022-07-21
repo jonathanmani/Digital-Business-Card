ac66b4e2fdb0b1529671823c9719e9bcb49660d1
Jonathan Mani <jonathanmani@protonmail.com>
2022-07-21 14:22:16 -0400

    added header information

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,8 @@ export default function Header(){
             <h1>Jonathan Mani</h1>
             <h3>Full Stack Developer</h3>
             <h4><a href='https://jonathanmani.netlify.app/'>Portfolio</a></h4>
+            <input type='button' className='button--email'>Email</input>
+            <input type='button' className='button--Linkedin'>Linkedin</input>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.css'
 import Photo from './components/Photo'
 import Header from './components/Header'
 
+
 export default function Page(){
     return(
-        <div>
+        <div className='container'>
             <Photo />
             <Header />
         </div>
