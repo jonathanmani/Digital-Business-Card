9398599b02cde8ac071fc693a3b416822fec60e7
Jonathan Mani <jonathanmani@protonmail.com>
2022-07-21 14:31:36 -0400

    finished Header componenent

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,10 @@ export default function Header(){
             <h1>Jonathan Mani</h1>
             <h3>Full Stack Developer</h3>
             <h4><a href='https://jonathanmani.netlify.app/'>Portfolio</a></h4>
-            <input type='button' className='button--email'>Email</input>
-            <input type='button' className='button--Linkedin'>Linkedin</input>
+            <div className='button-div'>
+                <button className='button-email'>Email</button>
+                <button className='button-linkedin'>Linkedin</button>
+            </div> 
         </div>
     )
 }
