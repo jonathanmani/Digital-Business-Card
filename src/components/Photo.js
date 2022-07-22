4fde86502ed7e26d1f02656540867eb4120e65a1
Jonathan Mani <jonathanmani@protonmail.com>
2022-07-22 11:47:19 -0400

    added the profile photo

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Photo(){
     return (
-        <img src={"./src/images/orange3.png"} alt='profile-pics' className='image'/>
+        <img src={require("../images/orange3.png")} alt={'profile-pics'} className='image'/>
     )
 }
